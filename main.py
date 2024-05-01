86707f7bdfdc615274541d3b90f62282353a0526
am4nr <am4n.ar@gmail.com>
2024-05-01 17:32:38 +0200

    implemented loading and formatting of tiled files -> maps and tilesets in json format

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 import scripts.utilities.flyweight as flyweight
 from scripts.utilities.settings import *
 import scripts.sprites.characters.main_character as main_character
+from scripts.sprites.tiles import Tileset, Tilemap
 
 
 class Game:
@@ -13,10 +14,20 @@ class Game:
         pygame.display.set_caption("Animal Adventure")
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.SCALED)
         self.sprites = []
-        self.player_image = flyweight.get_image_dict("characters", "finn\otter_idle_1.png")
-        self.player = main_character.Entity(50, 50, 0, 0, 0, 0, self.player_image["finn\otter_idle_1.png"])
+        self.player_image = flyweight.get_image_dict("characters", "finn\/finn_idle.png")
+        self.player = main_character.Entity(50, 50, 0, 0, 0, 0, self.player_image["finn\/finn_idle.png"])
         self.clock = pygame.time.Clock()
+        self.assets = {"tilesets":[],"maps":[]}
         
+        morning_adventure = Tileset("morning_adventure", self)
+        testmap = Tilemap("Test-Level", self)
+        
+        print(self.assets)
+        
+    def assetmanager(self, type, asset):
+        if type == "tileset" or type == "map":
+            self.assets[type+"s"].append(asset) 
+            
     def run(self):
         while True:
             # Events
@@ -35,4 +46,5 @@ class Game:
 
             pygame.display.flip()
             
-Game().run()
+game = Game()
+game.run()
