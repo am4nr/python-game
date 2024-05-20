1c70948652848502b54ffa6d36026160bdde314d
itsroj <rojdapolat@outlook.de>
2024-05-20 18:10:15 +0200

    init

--- scripts/gamestates.py
+++ scripts/gamestates.py
@@ -135,6 +135,7 @@ class Game:
         self.levels = [
             self.assets.get("Tilemap", "Test-Level"),
             self.assets.get("Tilemap", "Test-Level2"),
+            self.assets.get("Tilemap", "Level2"),
         ]
         self.current_level = 0
         # print(tracemalloc.get_traced_memory())
