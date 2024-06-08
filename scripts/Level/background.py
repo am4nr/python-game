2991e50d621ab3a1b471d4a062c739fcceff9f75
am4nr <am4n.ar@gmail.com>
2024-06-08 18:58:50 +0200

    removed unnecessary files

--- scripts/Level/background.py
+++ scripts/Level/background.py
@@ -1,12 +1,21 @@
 import pygame
 
 class Background:
-    def __init__(self, game, background_images, parallax_images):
+    def __init__(self, game, level_number):
         self.game = game
-        self.background_images = []
-        for image in background_images:
-            self.background_images.append(self.game.assets.get(image))
-        self.parallax_images = []
-        for image in self.parallax_images:
-            self.parallax_images.append(self.game.assets.get(image))
-    
+        self.file_prefix = "assets/background/"
+        self.file_suffix = "_0"+level_number+".png"
+        self.sky = self.game.assets.get("Image",self.file_prefix+"Sky"+self.file_suffix)
+        self.bg_decor = self.game.assets.get("Image",self.file_prefix+"BG_Decor"+self.file_suffix)
+        self.mid_decor = self.game.assets.get("Image",self.file_prefix+"Middle_Decor"+self.file_suffix)
+        self.fg_decor = self.game.assets.get("Image",self.file_prefix+"Foreground"+self.file_suffix)
+        self.ground = self.game.assets.get("Image",self.file_prefix+"Ground"+self.file_suffix)
+        print(f"Loaded Background for level {level_number}")
+        print(self.sky), print(self.bg_decor), print(self.mid_decor), print(self.fg_decor), print(self.ground)
+        
+    def draw(self):
+        self.game.screen.blit(self.sky, (0,0))
+        self.game.screen.blit(self.bg_decor, (0,0))
+        self.game.screen.blit(self.mid_decor, (0,0))
+        self.game.screen.blit(self.fg_decor, (0,0))
+        self.game.screen.blit(self.ground, (0,0))
