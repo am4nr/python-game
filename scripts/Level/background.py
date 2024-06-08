a25326e08e6ca9a2cd914f0929b7edb77a07af42
am4nr <am4n.ar@gmail.com>
2024-06-08 19:39:27 +0200

    background+parallax logic implemented

--- scripts/Level/background.py
+++ scripts/Level/background.py
@@ -1,21 +1,41 @@
+# This file contains the Background class which is responsible for loading and drawing the background images for the level
 import pygame
-
+import math
+from scripts.Utils.settings import HEIGHT
 class Background:
-    def __init__(self, game, level_number):
-        self.game = game
-        self.file_prefix = "assets/background/"
+    def __init__(self, level_number):
+        self.game = None
+        self.file_prefix = "background/"
+        self.level_number = level_number
         self.file_suffix = "_0"+level_number+".png"
+        self.offset = 0
+        self.offset_multiplier = {"sky": 0.5, "bg_decor": 0.75, "mid_decor": 1, "fg_decor": 1.25, "ground": 1.5}
+        
+    def load(self, game):
+        self.game = game
         self.sky = self.game.assets.get("Image",self.file_prefix+"Sky"+self.file_suffix)
         self.bg_decor = self.game.assets.get("Image",self.file_prefix+"BG_Decor"+self.file_suffix)
         self.mid_decor = self.game.assets.get("Image",self.file_prefix+"Middle_Decor"+self.file_suffix)
         self.fg_decor = self.game.assets.get("Image",self.file_prefix+"Foreground"+self.file_suffix)
         self.ground = self.game.assets.get("Image",self.file_prefix+"Ground"+self.file_suffix)
-        print(f"Loaded Background for level {level_number}")
+        print(f"Loaded Background for level {self.level_number}")
         print(self.sky), print(self.bg_decor), print(self.mid_decor), print(self.fg_decor), print(self.ground)
         
+    def update(self):
+        self.offset = self.game.level_manager.current_level.offset
+        
     def draw(self):
-        self.game.screen.blit(self.sky, (0,0))
-        self.game.screen.blit(self.bg_decor, (0,0))
-        self.game.screen.blit(self.mid_decor, (0,0))
-        self.game.screen.blit(self.fg_decor, (0,0))
-        self.game.screen.blit(self.ground, (0,0))
+        sky = pygame.transform.scale(self.sky, (self.sky.get_width()*0.8, HEIGHT))
+        bg_decor = pygame.transform.scale(self.bg_decor, (self.bg_decor.get_width()*0.8, HEIGHT))
+        mid_decor = pygame.transform.scale(self.mid_decor, (self.mid_decor.get_width()*0.8, HEIGHT))
+        fg_decor = pygame.transform.scale(self.fg_decor, (self.fg_decor.get_width()*0.8, HEIGHT))
+        
+        self.game.screen.blit(sky, (math.floor(self.offset*self.offset_multiplier["sky"]), 0))
+        self.game.screen.blit(sky, (math.floor(self.offset*self.offset_multiplier["sky"])-sky.get_width(), 0))
+        self.game.screen.blit(bg_decor, (math.floor(self.offset*self.offset_multiplier["bg_decor"]), 0))
+        self.game.screen.blit(bg_decor, (math.floor(self.offset*self.offset_multiplier["bg_decor"])-bg_decor.get_width(), 0))
+        self.game.screen.blit(mid_decor, (math.floor(self.offset*self.offset_multiplier["mid_decor"]), 0))
+        self.game.screen.blit(mid_decor, (math.floor(self.offset*self.offset_multiplier["mid_decor"])-mid_decor.get_width(), 0))
+        self.game.screen.blit(fg_decor, (math.floor(self.offset*self.offset_multiplier["fg_decor"]), 0))
+        self.game.screen.blit(fg_decor, (math.floor(self.offset*self.offset_multiplier["fg_decor"])-fg_decor.get_width(), 0))
+        self.game.screen.blit(self.ground, (math.floor(self.offset*self.offset_multiplier["ground"]), 0))
--- scripts/Level/levels.py
+++ scripts/Level/levels.py
@@ -1,11 +1,17 @@
 import pygame
 from scripts.Character.characters import Finn, Quack
 from scripts.Utils.settings import WIDTH
+from scripts.Level.background import Background
 
 vec = pygame.math.Vector2
+BG1 = Background("1")
+BG2 = Background("2")
+BG3 = Background("3")
+BG4 = Background("4")
+
 levels = [
-    {"level_name": "Test-Level", "character": Quack},
-    {"level_name": "Test-Level2", "character": Finn},
+    {"level_name": "Test-Level", "character": Quack, "background": BG1},
+    {"level_name": "Test-Level2", "character": Finn, "background": BG2},
 ]
 
 
@@ -39,20 +45,17 @@ class LevelManager:
         for level in levels:
             level_name = level.get("level_name")
             character = level.get("character")
+            background = level.get("background")
             character.game = self.game
             if len(self.levels) == 0:
-                self.current_level = Level(
-                    self.game, level_name, len(self.levels), character
-                )
+                self.current_level = Level(self.game, level_name, len(self.levels), character, background)
                 self.levels.append(self.current_level)
             else:
-                self.levels.append(
-                    Level(self.game, level_name, len(self.levels), character)
-                )
+                self.levels.append(Level(self.game, level_name, len(self.levels), character,background))
 
 
 class Level:
-    def __init__(self, game, level_name, level_id, character):
+    def __init__(self, game, level_name, level_id, character, background):
         self.game = game
         self.tilemap = game.assets.get("Tilemap", level_name)
         self.tilesets = self.tilemap.tilesets
@@ -63,6 +66,7 @@ class Level:
         self.moving_platforms = []
         self.character = character
         self.offset = 0
+        self.background = background
 
     def load(self):
         # if self.solid_layer:
@@ -71,6 +75,7 @@ class Level:
         self.tilemap.load_layers()
         self.moving_platforms = self.tilemap.layers["gameObjects"]["moving_platforms"]
         self.solid_layer = self.tilemap.layers["solid"]["group"]
+        self.background.load(self.game)
         for platform in self.moving_platforms:
             self.solid_layer.add(
                 platform
@@ -90,6 +95,7 @@ class Level:
         self.character.update()
         self.check_goal()
         self.set_offset()
+        self.background.update()
 
     def set_offset(self):
         # if (
@@ -124,6 +130,7 @@ class Level:
     def render(self):
         # print("render level")
         # Render the level tiles and objects
+        self.background.draw()
         for solid_tile in self.solid_layer:
             self.game.screen.blit(
                 solid_tile.image, (solid_tile.rect.x - self.offset, solid_tile.rect.y)
